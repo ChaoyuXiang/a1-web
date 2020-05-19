2552c7504ebb9d1dbf5c8a31d3ac11d8dd8984bf
ChaoyuXiang <chaoyuxiang07@gmail.com>
2020-05-18 23:14:36 -0700

    add welcome message

--- userInfo.js
+++ userInfo.js
@@ -7,4 +7,14 @@ const loadUser = function(){
     user.innerHTML = '';
     let curr = getCurr();
     user.textContent = `User: ${curr}`;
+    if(!localStorage.getItem(curr)){
+    localStorage.setItem(curr,JSON.stringify([welcome]));
+    }
+}
+
+const welcome = {
+    content: "Hi, I'm Chaoyu. You can post everything here at anytime. I hope you can enjoy it! 😊",
+    id:0,
+    state: ['happy'],
+    date: '2020/05/18'
 }
